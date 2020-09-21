290131ab0381f39e27aadab76023a00d4c5f89fa
moe <moe9999@protonmail.com>
2020-09-21 18:34:00 +0100

    Project structure and basic bot

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { config } = require("dotenv");
+const {
+  AkairoClient,
+  CommandHandler,
+  ListenerHandler,
+} = require("discord-akairo");
+const { join } = require("path");
+
+config();
+
+class ChronosClient extends AkairoClient {
+  constructor() {
+    super(
+      {
+        ownerID: process.env.OWNERID,
+      },
+      {
+        disableMentions: "all",
+      }
+    );
+
+    this.commandHandler = new CommandHandler(this, {
+      prefix: "+",
+      blockBots: true,
+      blockClient: true,
+      allowMention: true,
+      directory: join(__dirname, "commands"),
+    });
+
+    this.listnerHandler = new ListenerHandler(this, {
+      directory: join(__dirname, "listeners"),
+    });
+
+    this.commandHandler.useListenerHandler(this.listnerHandler);
+
+    this.listnerHandler.loadAll();
+    this.commandHandler.loadAll();
+  }
+}
+
+const client = new ChronosClient();
+client.login(process.env.TOKEN);
---
+++ src/listeners/ready.js
@@ -0,0 +1,16 @@
+const { Listener } = require("discord-akairo");
+
+class Ready extends Listener {
+  constructor() {
+    super("ready", {
+      event: "ready",
+      emitter: "client",
+    });
+  }
+
+  exec() {
+    console.log("Chronos is ready!");
+  }
+}
+
+module.exports = Ready;
