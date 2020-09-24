5a06d2e3df92cc37932e89b90fe884d372e87b94
Riday <ridayshah2001@gmail.com>
2020-09-24 19:26:02 +0530

    Fix: cleanup
    - Removed useless statements.

--- src/utilities/timeUtils.js
+++ src/utilities/timeUtils.js
@@ -1,21 +1,12 @@
 module.exports = {
   msToTime(ms) {
-    // Declare all variables.
-    let days = undefined;
-    let daysms = undefined;
-    let hours = undefined;
-    let hoursms = undefined;
-    let minutes = undefined;
-    let minutesms = undefined;
-    let sec = undefined;
-
-    days = Math.floor(ms / 86400000); // 24*60*60*1000
-    daysms = ms % 86400000; // 24*60*60*1000
-    hours = Math.floor(daysms / 3600000); // 60*60*1000
-    hoursms = ms % 3600000; // 60*60*1000
-    minutes = Math.floor(hoursms / 60000); // 60*1000
-    minutesms = ms % 60000; // 60*1000
-    sec = Math.floor(minutesms / 1000);
+    const days = Math.floor(ms / 86400000); // 24*60*60*1000
+    const daysms = ms % 86400000; // 24*60*60*1000
+    const hours = Math.floor(daysms / 3600000); // 60*60*1000
+    const hoursms = ms % 3600000; // 60*60*1000
+    const minutes = Math.floor(hoursms / 60000); // 60*1000
+    const minutesms = ms % 60000; // 60*1000
+    const sec = Math.floor(minutesms / 1000);
 
     let str = '';
     if (days) str = `${str + days}d`;
