226ad1a2133eccab5acb5fc7fb57ff2bdd1ec92e
codevenkat <codervenkat@gmail.com>
2020-10-04 16:19:41 +0530

    fix eslint errors

--- src/utilities/getGuildConfig.js
+++ src/utilities/getGuildConfig.js
@@ -1,13 +1,13 @@
 const config = require('../../config.json');
 
-module.exports = (guildID) => {
-	let guildConfig;
+module.exports = guildID => {
+  let guildConfig = undefined;
 
-	config.guildConfigurations.forEach((conf, i) => {
-		if (conf.guildID === guildID) {
-			guildConfig = conf;
-		}
-	});
+  config.guildConfigurations.forEach(conf => {
+    if (conf.guildID === guildID) {
+      guildConfig = conf;
+    }
+  });
 
-	return guildConfig;
-}
+  return guildConfig;
+};
